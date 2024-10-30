e80980da6917f600189183365adb4a5ac65a09c2
kovacssandorUI <sandorkovacs122@gmail.com>
2024-10-30 13:34:29 +0100

    add steps

--- src/step-definitions/Homepage_Steps.ts
+++ src/step-definitions/Homepage_Steps.ts
@@ -1,23 +1,33 @@
-import { Given, When, Then } from "@cucumber/cucumber";
-import { Browser, chromium, Page } from "playwright";
+import { Given, When } from "@cucumber/cucumber";
+import { chromium, Browser, Page } from "playwright";
 
-let browser: Browser; // Playwright browser instance
-let page: Page;
-let context: any;
+let browser: Browser; //Represents the browser instance (e.g., Chrome, Firefox) opened by Playwright.
+let context: any; //Represents a browser context (a separate browsing session); Each context has its own cookies, cache, and storage.
+let page: Page; //Represents a single web page within a context
 
-const url = "http://www.webdriveruniversity.com/";
+const url = "https://www.webdriveruniversity.com/";
 
-Given("I navigate the WebdriverUiversity homepage", async () => {
+Given("I navigate to the webdriveruniversity homepage", async () => {
+  //Setup browser instance:
   browser = await chromium.launch({ headless: false });
   context = await browser.newContext({
     viewport: { width: 1920, height: 1080 },
   });
   page = await context.newPage();
+
+  //Access URL
   await page.goto(url);
 });
 
 When("I click on the contact us button", async () => {
-  await page.pause();
-  const contactUsButton = await page.$("#contact-us");
-  await contactUsButton?.click();
+  //await page.pause();
+  const contactUs_Button = await page.getByRole("link", {
+    name: "CONTACT US Contact Us Form",
+  });
+  await contactUs_Button.click();
+});
+
+When("I switch to the new browser tab", async () => {
+  page = await context.waitForEvent("page"); //reintialise the page > new tab > page
+  await page.bringToFront();
 });
