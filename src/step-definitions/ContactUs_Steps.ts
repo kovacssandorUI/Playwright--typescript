403dec5578515d9d938fac3722769be0cda05f69
kovacssandorUI <sandorkovacs122@gmail.com>
2024-10-31 15:21:24 +0100

    add faker_js

--- src/step-definitions/ContactUs_Steps.ts
+++ src/step-definitions/ContactUs_Steps.ts
@@ -1,6 +1,7 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import { pageFixture } from "./hooks/browserContextFixture";
 import { expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
 
 When("I type a first name", async () => {
   await pageFixture.page.getByPlaceholder("First Name").fill("Joe");
@@ -31,7 +32,7 @@ When("I click on the submit button", async () => {
 Then(
   "I should be presented with a successful contact us submission message",
   async () => {
-    //wait for the header text element
+    //waiting for the header text element
     await pageFixture.page.waitForSelector("#contact_reply h1", {
       timeout: 60000,
     });
@@ -43,3 +44,62 @@ Then(
     expect(text).toBe("Thank You for your Message!");
   }
 );
+
+Then(
+  "I should be presented with a unsuccessful contact us message",
+  async () => {
+    //wait for the <body> element
+    await pageFixture.page.waitForSelector("body");
+
+    //Locate the <body> element
+    const bodyElement = await pageFixture.page.locator("body");
+
+    //Extract text from the element
+    const bodyText = await bodyElement.textContent();
+
+    await expect(bodyText).toMatch(
+      /Error: (all fields are required|Invalid email address)/
+    );
+  }
+);
+
+//Cucumber Expressions:
+When("I type a specific first name {string}", async (firstName: string) => {
+  await pageFixture.page.getByPlaceholder("First Name").fill(firstName);
+});
+
+When("I type a specific last name {string}", async (lastName: string) => {
+  await pageFixture.page.getByPlaceholder("Last Name").fill(lastName);
+});
+
+When(
+  "I enter a specific email address {string}",
+  async (emailAddress: string) => {
+    await pageFixture.page.getByPlaceholder("Email Address").fill(emailAddress);
+  }
+);
+
+When(
+  "I type specific text {string} and a number {int} within the comment input field",
+  async (word: string, number: number) => {
+    await pageFixture.page
+      .getByPlaceholder("Comments")
+      .fill(word + " " + number);
+  }
+);
+
+//Random Data - Faker
+When("I type a random first name", async () => {
+  const randomFirstName = faker.person.firstName();
+  await pageFixture.page.getByPlaceholder("First Name").fill(randomFirstName);
+});
+
+When("I type a random last name", async () => {
+  const randomLastName = faker.person.lastName();
+  await pageFixture.page.getByPlaceholder("Last Name").fill(randomLastName);
+});
+
+When("I enter a random email address", async () => {
+  const randomEmail = faker.internet.email();
+  await pageFixture.page.getByPlaceholder("Email Address").fill(randomEmail);
+});
